7db427bd5b56cb2c858035299b49af4c4d766a51
Kaan <92580778+GitHubKaan@users.noreply.github.com>
2026-02-25 19:22:10 +0100

    feat: imprint, policy rework

--- src/pages/privacy.page.tsx
+++ src/pages/privacy.page.tsx
@@ -4,8 +4,76 @@ function PrivacyPage() {
     return (
         <div id="privacy-page" className="default-page">
             <h2>Privacy</h2>
-            <div className="w-100 flex">
-                <p>Lorem ipsum dolor sit amet, sis aeirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.    Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.    Duis autem vel eum iriure dolor in</p>
+            <div className="w-100 flex column">
+                <h1>Privacy Policy</h1>
+                <p>Data protection</p>
+                <h3 className="mb-1 mt-1">§ 1 Information on the collection of personal data</h3>
+                <p>(1) In the following we inform you about the collection of personal data when using our website. Personal data includes all data that can be related to you personally, e.g. name, address, e-mail addresses, user behavior.</p>
+                <br />
+                <p>(2) Responsible according to Art. 4 para. 7 EU Data Protection Basic Regulation (DS-GVO) is</p>
+                <br />
+                <p>Card Sports League GmbH</p>
+                <p>Kieler Straße 3</p>
+                <p>15732 Schulzendorf</p>
+                <p>Amtsgericht: Cottbus</p>
+                <p>Handelsregister: HRB 16886 CB</p>
+                <p>Norman Wilde (norman@csl.tv),</p>
+                <p>Ali Fawaz (ali@csl.tv)</p>
+                <br />
+                <p>(3) When you contact us by e-mail or via a contact form, we will store the data you provide (your e-mail address, possibly your name and telephone number) in order to answer your questions. We will delete the data that arises in this context after the storage of such is no longer required, or restrict processing in the case that there are legal storage obligations.</p>
+                <br />
+                <p>(4) If we use contracted service providers for individual functions of our offer or would like to use your data for advertising purposes, we will inform you below in detail about the respective processes. We will also specify the criteria for the storage period.</p>
+                <h3 className="mb-1 mt-1">§ 2 Your rights</h3>
+                <p>(1) You have the following rights towards us with regard to your personal data:</p>
+                <br />
+                <p>Right to information,</p>
+                <p>Right to rectification or deletion,</p>
+                <p>Right to restrict processing,</p>
+                <p>Right to object to processing, § 5,</p>
+                <p>Right to data transferability.</p>
+                <br />
+                <p>(2) You also have the right to complain to a data protection supervisory authority about the processing of your personal data by us.</p>
+                <h3 className="mb-1 mt-1">§ 3 Collection of personal data when you visit our website</h3>
+                <p>(1) If you are using the website for informational purposes only, i.e. if you do not register or otherwise provide us with information, we only collect the personal data that your browser transmits to our server. If you wish to view our website, we collect the following data, which is technically necessary for us to display our website to you and to guarantee stability and security (legal basis is Art. 6 Para. 1 S. 1 lit. f DS-GVO):</p>
+                <br />
+                <p>IP address</p>
+                <p>Data and time of inquiry</p>
+                <p>Time zone difference to Greenwich Mean Time (GMT)</p>
+                <p>Content of the request (specific page)</p>
+                <p>Access status/HTTP status code</p>
+                <p>Amount of data transferred in each case</p>
+                <p>Website from which the request originates</p>
+                <p>Browser</p>
+                <p>Operating system and its interface</p>
+                <p>Language and version of browser software.</p>
+                <br />
+                <p>(2) In addition to the aforementioned data, cookies are stored on your terminal device when you use our website. Cookies are small text files that are stored in your memory assigned to the browser you are using and through which certain information flows to the location that sets the cookie (in this case to us). Cookies cannot execute programs or transmit viruses to your computer. They serve to make the website more user-friendly and effective.</p>
+                <br />
+                <p>(3) Use of cookies:</p>
+                <br />
+                <p>a) This website uses the following types of cookies, the scope and functionality of which are explained below:</p>
+                <br />
+                <p>Transient cookies (to b)</p>
+                <br />
+                <p>Persistent cookies (to c).</p>
+                <br />
+                <p>b) Transient cookies are automatically deleted when you close your browser. These include in particular session cookies. They store a so-called session ID, which can be used to assign various requests from your browser to the shared session. This enables your computer to be recognized when you return to our website. The session cookies are deleted when you log out or close your browser.</p>
+                <br />
+                <p>c) Persistent cookies are automatically deleted after a specified period, which may vary depending on the cookie. You can delete cookies at any time in the security settings of your browser.</p>
+                <br />
+                <p>d) You can configure your browser settings according to your wishes and, for example, refuse the acceptance of third-party cookies or all cookies. We would like to point out that you then may not be able to use all the functions of this website.</p>
+                <br />
+                <p>e) We use cookies to identify you for subsequent visits if you have an account with us. Otherwise you will have to log in again for each visit.</p>
+                <br />
+                <p>f) The Flash cookies used are not captured by your browser, but by your Flash plug-in. We also use HTML5 storage objects that are stored on your device. These objects store the required data regardless of the browser you are using and do not have an automatic expiration date. If you do not want the Flash cookies to be processed, you must install an appropriate add-on, e.g. “Better Privacy” for Mozilla Firefox (https://addons.mozilla.org/de/firefox/addon/betterprivacy/) or the Adobe Flash Killer cookie for Google Chrome. You can prevent the use of HTML5 storage objects by using private mode in your browser. We also recommend that you regularly delete your cookies and browser history manually.</p>
+                <h3 className="mb-1 mt-1">§ 4 Data security</h3>
+                <p>Within the scope of the website visit, we use the common SSL (Secure Socket Layer) method in connection with the highest level of encryption supported by your browser. This is generally a 256-bit encryption. If your browser does not support 256-bit encryption, we use 128-bit v3 technology instead. You can tell whether an individual page of our website is transmitted in encrypted form by the closed representation of the key or lock symbol in the lower status bar of your browser.</p>
+                <br />
+                <p>We also use appropriate technical and organizational security measures to protect your data against accidental or intentional manipulation, partial or complete loss, destruction or against unauthorized access by third parties. Our security measures are continuously improved in line with technological developments.</p>
+                <h3 className="mb-1 mt-1">§ 5 Right to objection</h3>
+                <p>If your personal data are processed on the basis of legitimate interests pursuant to Art. 6 para. 1 sentence 1 lit. f DSGVO, you have the right, pursuant to Art. 21 DSGVO, to object to the processing of your personal data if there are reasons for doing so which arise from your particular situation or which are directed against direct advertising. In the latter case, you have a general right of objection, which is implemented by us without having to state a particular situation. If you would like to exercise your right to revocation or objection, all you have to do is send an e-mail to norman.wilde@cardsportsleague.com.</p>
+                <h3 className="mb-1 mt-1">§ 6 Actuality and changes to this data protection declaration</h3>
+                <p>This data protection declaration is currently valid and as of November 2018. Due to the further development of our website and offers on it or due to changed legal or official requirements, it may be necessary to amend this data protection declaration. The respective valid data protection declaration can be called up at any time on our website at http://cardsportsleague.com/data-protection and printed out.</p>
             </div>
         </div>
     );
