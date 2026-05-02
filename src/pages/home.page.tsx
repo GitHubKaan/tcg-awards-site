6e028aef55c810b12da64593cf60066618a46be6
Kaan <92580778+GitHubKaan@users.noreply.github.com>
2026-05-02 02:35:57 +0200

    fix

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -218,7 +218,7 @@ function HomePage() {
                     <FrameComponent
                         image={Logo2}
                         title={{
-                            top: "local\ngame",
+                            top: "local\ngame store",
                             bottom: "of the year"
                         }}
                     />
