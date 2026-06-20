9079180f91bd6d07c753e67fed04457e42cd5a4d
Kaan <92580778+GitHubKaan@users.noreply.github.com>
2026-06-20 15:24:36 +0200

    change

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -242,7 +242,7 @@ function HomePage() {
                 
                 <div className="flex column gap-1">
                     <div className="flex gap column center items-center">
-                        <VoteButtonComponent title={`Register your Brand Case`} onClick={() => navigate("/brand-cases-info")} />
+                        <VoteButtonComponent title={`More info`} onClick={() => navigate("/brand-cases-info")} />
                     </div>
                     
                     <div className="w-100 flex center items-center">
